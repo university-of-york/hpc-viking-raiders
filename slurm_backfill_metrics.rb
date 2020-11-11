21b1ed6c4390105d637f460c431c168b79a65eb0
Killian Murphy <killian.murphy@york.ac.uk>
2020-11-11 15:56:48 +0000

    Template for backfill raider

---
+++ slurm_backfill_metrics.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Pull backfill scheduler metrics from sdiag and report
+class SlurmPendingOnQos
+  def initialize(collector, config)
+    @collector = collector
+    @config = config
+  end
+
+  def raid
+  end
+end
