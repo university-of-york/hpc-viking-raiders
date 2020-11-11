bc57c085e987f831ee50798109d6bc114182c22c
Nat Lasseter <nat.lasseter@york.ac.uk>
2020-11-11 14:57:09 +0000

    New raider: number of jobs by partition, user, and state

---
+++ slurm_number_jobs.rb
@@ -0,0 +1,31 @@
+class SlurmNumberJobs
+  def initialize(collector, config)
+    @collector = collector
+    @config = config
+  end
+
+  def raid
+    raw = `squeue --format="%P,%u,%T" --noheader`
+    raw = raw.lines
+    raw = raw.map(&:strip)
+    raw = raw.map{ |line| line.split(',') }
+
+    tally = raw.tally
+
+    @collector.redact!("slurm_number_jobs")
+
+    tally.each do |labelset, number|
+      @collector.report!(
+        "slurm_number_jobs",
+        number,
+        help: "Number of jobs for a given user, partition, and state",
+        type: "gauge",
+        labels: {
+          partition: labelset[0],
+          user: labelset[1],
+          state: labelset[2]
+        }
+      )
+    end
+  end
+end
