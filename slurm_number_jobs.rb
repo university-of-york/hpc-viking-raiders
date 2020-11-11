fdc0cc08592e4606e684951b5e2503832b15d148
Nat Lasseter <nat.lasseter@york.ac.uk>
2020-11-11 15:43:56 +0000

    slurm_number_jobs rubocop fixes

--- slurm_number_jobs.rb
+++ slurm_number_jobs.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+# Report the number of jobs currently in queue,
+# aggregated by state, user, and partition
 class SlurmNumberJobs
   def initialize(collector, config)
     @collector = collector
@@ -6,20 +10,19 @@ class SlurmNumberJobs
 
   def raid
     raw = `squeue --format="%P,%u,%T" --noheader`
-    raw = raw.lines
-    raw = raw.map(&:strip)
-    raw = raw.map{ |line| line.split(',') }
+    raw = raw.lines.map(&:strip)
+    raw = raw.map { |line| line.split(',') }
 
     tally = raw.tally
 
-    @collector.redact!("slurm_number_jobs")
+    @collector.redact!('slurm_number_jobs')
 
     tally.each do |labelset, number|
       @collector.report!(
-        "slurm_number_jobs",
+        'slurm_number_jobs',
         number,
-        help: "Number of jobs for a given user, partition, and state",
-        type: "gauge",
+        help: 'Number of jobs for a given user, partition, and state',
+        type: 'gauge',
         labels: {
           partition: labelset[0],
           user: labelset[1],
