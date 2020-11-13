fe5a326c49bcf8f08faef8a5aa037bbbc0ee7cb0
Jasper Grimm <jasper.grimm@york.ac.uk>
2020-11-13 12:32:19 +0000

    Extend Array class with mean, median methods Skip calculation if no jobs match criteria Complete binning queue time by cores requested Calculate mean/median/max queue time for each bin

--- slurm_queue_time_cores_requested.rb
+++ slurm_queue_time_cores_requested.rb
@@ -1,5 +1,21 @@
 require 'date'
 
+# extend Array to support mean, median
+class Array
+  def mean(array)
+    return nil if array.empty?
+    array = array.sum.fdiv(array.size)
+  end
+
+  def median(array)
+    return nil if array.empty?
+    array = array.sort
+    mid = array.size / 2  # midpoint
+    # if length is odd, select middle element; otherwise take mean of two middle elements
+    return array.size % 2 == 1 ? array[mid] : mean(array[mid-1..mid])
+  end
+end
+
 class SlurmQueueTimesCoresRequested
   def initialize(collector, config)
     @collector = collector
@@ -12,35 +28,74 @@ class SlurmQueueTimesCoresRequested
 
     sacct_cmd = [
       'sacct',
-      '-a',                     # all jobs
-      '-X',                     # allocations
-      '-P',                     # "|" - delimited 
+      '-a', # all jobs
+      '-X', # allocations
+      '-P', # "|" - delimited 
       '--partition=nodes',
-      '--state=CD',
-      '-o AllocCPUs,Submit,Start',  
-      '-S #{start_time}',
+      '--state=CD,CF,CG,DL,F,NF,OOM,PR,R,ST,TO',  # consider all job states that have had resources allocated since the
+      '-o AllocCPUs,Submit,Start',                # last cycle
+      '-S #{start_time}', 
       '-E #{end_time}'
     ].join(' ')
     
     # get raw data from sacct and read jobs into an array
     data = `#{squeue_cmd}`.lines
     # remove any whitespace from the ends of each string
-    data = data.map(&:strip)
+    data.map!(&:strip)
     # drop the header line
     data = data[1..-1]
-    # split each line by observables
-    data = data.map{ |row| row.split("|") }
-    # calculate queue time from (start - submit) time
-    data = data.map{ |job| job = [job[0].to_i, (DateTime.parse(job[2]).to_time - DateTime.parse(job[1]).to_time).to_i] }
-    
-    # create bin intervals (AllocCPUs)
-    bins_cores = [1, 10, 30, 100, 250, 500] # upper bin boundaries
-    bins_cores.prepend(-1).each_cons(2) { |lower, upper| [lower+1, upper] } # prepend -1 to ensure first bin starts at 0
+    # number of jobs considered
+    njobs = data.length()
 
-    # bin queue time by core
-    cores_bins.map do |bin|
-      data.select{ |cores, time| cores.between?(bin[0], bin[1]) }.map{ |job| job[1] }
+    # don't bother calculating if this is 0
+    if njobs > 0
+      # split each line by observables
+      data.map! { |row| row.split("|") }
+      
+      # calculate queue time from (start - submit) time
+      data.map! do |job|
+        job = [job[0].to_i,
+               (DateTime.parse(job[2]).to_time - DateTime.parse(job[1]).to_time).to_i]
+      end
+      
+      # create bin intervals (AllocCPUs)
+      bins_cores = [1, 10, 30, 100, 250, 500]                  # upper bin boundaries
+                     .prepend(-1)                              # prepend -1 to ensure first bin starts at 0
+                     .each_cons(2)                             # consider consecutive pairs
+                     .map { |lower, upper| [lower+1, upper] }  # generate bin lower/upper limit pairs
+      
+      # bin queue time by core
+      binned_by_core = bins_cores.map do |bin|
+        data
+          .select{ |cores, time| cores.between?(bin[0], bin[1]) } # find matching jobs for bin
+          .map{ |job| job[1] }                                    # queue time
+      end
+      
+      qt_by_core = binned_by_core.map do |bin|
+        mean_qt = bin.mean
+        median_qt = bin.median
+        max_qt = bin.max
+        bin = [mean_qt, median_qt, max_qt]
+      end
+
+      qt_by_core.each do |mean, median, max|
+        @collector.report!(
+          "queue_time_cores_requested",
+          
+        )
+      end
+
+
+    else
+      # no jobs
     end
+  end
+end
+
+   
+
+
+
 
 
 
