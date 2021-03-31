c66cd2a7690ebdedcd9438bb4e099e95c3d3bbf8
Jasper Grimm <jasper.grimm@york.ac.uk>
2021-03-31 15:39:33 +0100

    Linting Fix regex

--- slurm_scheduler_statistics.rb
+++ slurm_scheduler_statistics.rb
@@ -6,13 +6,13 @@ class SlurmSchedulerStatistics
     @collector = collector
     @config = config
     @sdiag_categories = %w[General Main Backfilling RPC]
-    
+
     # Regex to split sdiag output into General/Main scheduler/Backfilling/RPC
-    @sdiag_split_regex = /^Main schedule statistics \(microseconds\):|
-                          ^Backfilling stats|
-                          ^Remote Procedure Call statistics by message type/x
+    @sdiag_split_regex = /^Main\sschedule\sstatistics\s\(microseconds\):|
+                          ^Backfilling\sstats|
+                          ^Remote\sProcedure\sCall[\s\w]+type/x
     @sdiag_parse_regex = /^\s*([\w \t()]+):\s+(\d+)/
-    
+
     @stats_to_report = {
       'General' => [],
       'Main' => [
@@ -40,7 +40,7 @@ class SlurmSchedulerStatistics
 
   def array_to_categorised_hash(categories, data)
     zipped_array = categories.zip(data)
-    categorised_hash = zipped_array.to_h
+    zipped_array.to_h
   end
 
   def parse_hash(sdiag_hash, pattern)
@@ -49,7 +49,7 @@ class SlurmSchedulerStatistics
       sdiag_hash[category] = matches
     end
 
-    hash
+    sdiag_hash
   end
 
   def filter_stats(stats, filter_hash)
@@ -59,34 +59,36 @@ class SlurmSchedulerStatistics
       # keep only statistics listed in the filter
       category_stats.keep_if { |stat, _| filter.include? stat }
     end
+    # strip empty categories
+    stats.reject! { |_, stat| stat.empty? }
+  end
+
+  def report_category_stats(category, category_stats)
+    category_stats.each do |stat, val|
+      help = "Slurm scheduler (#{category.downcase}): #{stat}"
+      description = help.downcase.gsub(/\s+/, '_').gsub(/[():]/, '')
+      @collector.report!(
+        description,
+        val,
+        help: help,
+        type: 'gauge'
+      )
+    end
   end
 
   def raid
     sdiag = `sdiag`
-    
+
     sdiag_split = sdiag.split(@sdiag_split_regex)
 
     sdiag_hash = array_to_categorised_hash(@sdiag_categories, sdiag_split)
-    
-    stats = parse_hash(sdiag_hash)
 
-    stats = filter_stats(stats, stats_to_report) 
+    stats = parse_hash(sdiag_hash, @sdiag_parse_regex)
+
+    stats = filter_stats(stats, @stats_to_report)
 
     stats.each do |category, category_stats|
-      next unless category_stats.empty?
-
-      category_lower = category.downcase
-      category_stats.each do |stat, val|
-        help = "Slurm scheduler (#{category}): #{stat}"
-        description = help.downcase.gsub(/\s+/, '_').gsub(/[():]/, '')
-        
-        @collector.report!(
-          description,
-          val,
-          help: help,
-          type: gauge
-        )
-      end
+      report_category_stats(category, category_stats)
     end
   end
 end
